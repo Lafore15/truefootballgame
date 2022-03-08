60bfaa610995e7bda99953eb3016ccc120b79549
Lafore15 <mkov2n1517@gmail.com>
2022-03-08 09:49:10 +0300

    refactored

--- tournament.py
+++ tournament.py
@@ -8,8 +8,8 @@ class Tournament:
     def __init__(self):
         self.buckets = {'first bucket': [], 'second bucket': []}
         self.groups = {'Group A': [], 'Group B': []}
-        self.quarterfinalists1 = []
-        self.quarterfinalists2 = []
+        self.quarter_finalists1 = []
+        self.quarter_finalists2 = []
         self.semifinalists = []
         self.champion = []
 
@@ -32,9 +32,9 @@ class Tournament:
         a_group = copy.deepcopy(self.groups['Group A'])
         b_group = copy.deepcopy(self.groups['Group B'])
         already_played = []
-        semifinalists = []
-        quarterfinalists1 = []
-        quarterfinalists2 = []
+        quarter_finalists1 = []
+        quarter_finalists2 = []
+        semi_finalists = []
         for i in range(4):
             match1 = Match()
             already_played.clear()
@@ -43,24 +43,27 @@ class Tournament:
             a_group.remove(already_played[0])
             b_group.remove(already_played[1])
             if i % 2 == 0:
-                quarterfinalists1.append(match1.winner)
+                quarter_finalists1.append(match1.winner)
             else:
-                quarterfinalists2.append(match1.winner)
+                quarter_finalists2.append(match1.winner)
             del match1
-        self.quarterfinalists1 = copy.deepcopy(quarterfinalists1)
-        self.quarterfinalists2 = copy.deepcopy(quarterfinalists2)
+        self.quarter_finalists1 = copy.deepcopy(quarter_finalists1)
+        self.quarter_finalists2 = copy.deepcopy(quarter_finalists2)
         for _ in range(2):
             match1 = Match()
             already_played.clear()
-            match1.get_match([random.choice(quarterfinalists1), random.choice(quarterfinalists2)])
+            match1.get_match([random.choice(quarter_finalists1), random.choice(quarter_finalists2)])
             already_played.extend(match1.group)
-            quarterfinalists1.remove(already_played[0])
-            quarterfinalists2.remove(already_played[1])
-            semifinalists.append(match1.winner)
+            quarter_finalists1.remove(already_played[0])
+            quarter_finalists2.remove(already_played[1])
+            semi_finalists.append(match1.winner)
             del match1
         grand_final = Match()
-        grand_final.get_match(semifinalists)
-        return grand_final.get_winner().team, [i.team  for i in semifinalists], [i.team for i in self.quarterfinalists1], [i.team for i in self.quarterfinalists2]
+        grand_final.get_match(semi_finalists)
+        return grand_final.get_winner().team, [i.team for i in semi_finalists], [i.team for i in self.quarter_finalists1], [i.team for i in self.quarter_finalists2]
 
 
-
+champions_league = Tournament()
+champions_league.create_groups([ip.juventus, ip.bayern, ip.inter, ip.man_united, ip.chelsea, ip.barca, ip.real, ip.liver])
+print(champions_league.get_groups())
+print(champions_league.create_playoff())
